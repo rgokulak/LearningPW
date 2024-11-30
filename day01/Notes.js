fa2e20113471ad7359b72172caccbad4237f2bab
Gokulakrishnan Ravishankar <rgokan279@gmail.com>
2024-11-30 15:41:10 +0530

    Add files via upload

---
+++ day01/Notes.js
@@ -0,0 +1,43 @@
+
+//To print statement 
+console.log("Hello");
+
+console.log("Lovely Saturday");
+
+var age = 30;
+console.log(typeof age);
+
+var phone1 = 9810;
+var phone2 = '0';
+console.log(phone1);
+console.log(phone2);
+
+//boolean
+var isJavaScriptInteresting = true;
+console.log(typeof isJavaScriptInteresting);
+
+//undefined
+var accnumber;
+console.log(accnumber);
+console.log(typeof accnumber);
+accnumber = "00123";
+console.log(typeof accnumber);
+
+//null
+var email = null;
+console.log(email);
+console.log(typeof email);
+
+//declaratin of variables
+let company = "Apple";
+company = "Amazon";
+console.log(company);
+
+const password = "qwerty";
+//reclaration and reassignment not possbile
+
+//Scoping - Global, Function, Block Level
+function printGender() {
+
+}
+
---
+++ day02/AssignementOne24nov.js
@@ -0,0 +1,12 @@
+//var vs let vs const
+
+const browserVersion="Chrome";
+
+function getBrowserVersion() {
+    if (browserVersion==="Chrome") {
+        var AbrowserVersion ="Using Chrome Browser";
+        console.log(AbrowserVersion);
+    }
+}
+getBrowserVersion();
+
